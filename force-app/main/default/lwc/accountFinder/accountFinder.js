baf663dc4ee73ca126831aa284907497ddd2c763
bitdern <acnewell09@gmail.com>
2023-09-25 15:48:21 -0500

    custom class to sort by revenue

--- force-app/main/default/lwc/accountFinder/accountFinder.js
+++ force-app/main/default/lwc/accountFinder/accountFinder.js
@@ -1,4 +1,5 @@
-import { LightningElement } from "lwc";
+import { LightningElement, wire } from "lwc";
+import queryAccountsByRevenue from "@salesforce/apex/AccountListControllerLwc.queryAccountsByRevenue";
 
 export default class AccountFinder extends LightningElement {
   annualRevenue = null;
@@ -9,4 +10,8 @@ export default class AccountFinder extends LightningElement {
   reset() {
     this.annualRevenue = null;
   }
+  @wire(queryAccountsByRevenue, {
+    annualRevenue: "$annualRevenue"
+  })
+  accounts;
 }
